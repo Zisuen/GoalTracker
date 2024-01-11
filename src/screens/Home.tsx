fbdc2c2d51eaa6d8aa97188cdac5a398de77bb23
Zisuen <urbanovsky97@gmail.com>
2024-01-11 16:20:37 +0100

    Supabase Setup
    Supabase has been setup

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
+import supabase from '../service/api';
 
 const Home = () => {
+  const [fetchError, setFetchError] = useState<null | string>(null);
+  const [goals, setGoals] = useState<null | any[]>(null);
+
+  useEffect(() => {
+    const fetchGoals = async () => {
+      const {data, error} = await supabase.from('goals').select();
+      if (error) {
+        setFetchError('FetchError');
+        setGoals(null);
+        console.log(error);
+      }
+      if (data) {
+        setGoals(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchGoals();
+  }, []);
+
   return (
-    <View>
+    <View style={{marginTop: 50, marginHorizontal: 20}}>
       <Text>Home</Text>
+      {fetchError && <Text>{fetchError}</Text>}
+      {goals && (
+        <View>
+          {goals.map(goal => (
+            <Text key={goal.id}>{goal.goal_title}</Text>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
---
+++ src/service/api.ts
@@ -0,0 +1,9 @@
+import 'react-native-url-polyfill/auto';
+import {createClient} from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://tmolyqslvghkivdbfpqu.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtb2x5cXNsdmdoa2l2ZGJmcHF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk1Mzk0ODUsImV4cCI6MjAxNTExNTQ4NX0.KQyjrq4YrfiEVUpsRTAb08dCZDT459twIXfEoSalu5s';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
