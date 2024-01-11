aac61a732af833383c482e94570e799add9a4ed3
Zisuen <urbanovsky97@gmail.com>
2024-01-11 11:13:26 +0100

    Navigation
    Navigation insstalled

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import Home from './screens/Home';
 
+const RootStack = createNativeStackNavigator();
+
+const RootStackNavigator = () => {
+  return (
+    <RootStack.Navigator screenOptions={{headerShown: false}}>
+      <RootStack.Screen name="Home" component={Home} />
+    </RootStack.Navigator>
+  );
+};
+
 const App = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <RootStackNavigator />
+    </NavigationContainer>
+  );
 };
 
 export default App;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text} from 'react-native';
 
 const Home = () => {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Home</Text>
     </View>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
