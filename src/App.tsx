1c8d1c47f6cc24a7a5ced81625ad08ee1e352d76
Zisuen <urbanovsky97@gmail.com>
2024-01-11 10:14:06 +0100

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import Home from './screens/Home';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>App Test</Text>
-    </View>
-  );
+  return <Home />;
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
