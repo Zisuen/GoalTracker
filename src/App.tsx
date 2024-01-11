7048e5c42c2bd5088961209ed3c369c3fc798883
Zisuen <urbanovsky97@gmail.com>
2024-01-11 10:10:11 +0100

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {View, Text, StyleSheet} from 'react-native';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>App</Text>
+      <Text>App Test</Text>
     </View>
   );
 };
