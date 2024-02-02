a42a418ff57c4a31d87df0c353857fe115543f12
Zisuen <urbanovsky97@gmail.com>
2024-02-02 12:39:27 +0100

    Starting anew

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,20 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {useSelector} from 'react-redux';
-import {Provider} from 'react-redux';
-import {RootState, store} from './redux/store';
-import Account from './screens/Account';
-import Goals from './screens/Goals';
-import Habits from './screens/Habits';
-import Login from './screens/Login';
-import ToDo from './screens/ToDo';
-
-const RootStack = createNativeStackNavigator();
-
-const RootStackNavigator = () => {
-  return (
-    <RootStack.Navigator
-      screenOptions={{headerShown: false, animation: 'none'}}>
-      <RootStack.Screen name="Goals" component={Goals} />
-      <RootStack.Screen name="Habits" component={Habits} />
-      <RootStack.Screen name="To-Do" component={ToDo} />
-      <RootStack.Screen name="Account" component={Account} />
-    </RootStack.Navigator>
-  );
-};
-
-const LoginStack = createNativeStackNavigator();
-
-const LoginStackNavigator = () => {
-  return (
-    <LoginStack.Navigator screenOptions={{headerShown: false}}>
-      <LoginStack.Screen name="Login" component={Login} />
-    </LoginStack.Navigator>
-  );
-};
-
-const SecurityNavigator = () => {
-  const {isLoggedIn} = useSelector((state: RootState) => state.user);
-  if (isLoggedIn) {
-    return <RootStackNavigator />;
-  } else {
-    return <LoginStackNavigator />;
-  }
-};
+import {View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <SecurityNavigator />
-      </NavigationContainer>
-    </Provider>
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
