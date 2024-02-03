d9ca4b7a03847b65ff658a4a7cc9591dec0c7cc9
Zisuen <urbanovsky97@gmail.com>
2024-02-03 19:38:28 +0100

    ThemeContext implemented

---
+++ src/config/theme/theme.ts
@@ -0,0 +1,23 @@
+export type THEME = {
+  background: string;
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  text: string;
+};
+
+export const dark: THEME = {
+  background: '#000000',
+  primary: '#414c7c',
+  secondary: '#4f4f4f',
+  tertiary: '#214229',
+  text: '#ffffff',
+};
+
+export const light: THEME = {
+  background: '#f0fdd3',
+  primary: '#aec456',
+  secondary: '#78e0d7',
+  tertiary: '#d782c8',
+  text: '#000000',
+};
