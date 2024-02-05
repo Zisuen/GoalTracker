e2e3d518724bc441674c036410b028d287829390
Zisuen <urbanovsky97@gmail.com>
2024-02-05 17:36:35 +0100

    Navigation updated

--- src/components/Login/SignUpBtn.tsx
+++ src/components/Login/SignUpBtn.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
+import {NavigationProp} from '@react-navigation/native';
 
 import stylesSignUpBtn from '~/config/styles/components/Login/SignUpBtn.styles';
+import {AuthenticationStackParams} from '~/config/types/api.types';
 
-const SignUpBtn = () => {
+type SIGN_UP_BTN = {
+  navigation: NavigationProp<AuthenticationStackParams>;
+};
+
+const SignUpBtn = ({navigation}: SIGN_UP_BTN) => {
   const styles = stylesSignUpBtn();
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Don't have an account yet ?</Text>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
         <Text style={styles.btnLabel}>Sign Up</Text>
       </TouchableOpacity>
     </View>
--- src/screens/Auth/SignUp.tsx
+++ src/screens/Auth/SignUp.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const SignUp = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>SignUp</Text>
     </View>
   );
 };
 
 export default SignUp;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
